119caaad05be057ff44da8e3b7660a270a7e9e59
nanut <galunyu@gmail.com>
2019-10-18 18:28:16 +0700

    form req.params

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.get('/api/courses', (req, res) => res.send([1, 2, 3]))
+
+app.get('/api/courses/:id', (req, res) => res.send(req.params.id))
+
+app.get('/api/posts/:year/:month', (req, res) => res.send(req.params))
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Listening on prot ${port}...`))
